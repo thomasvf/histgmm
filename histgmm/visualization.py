e37fc02f0dbb11593680527c6ba2f3cf79b17d5a
thomasvf <tvfontanari@gmail.com>
2024-02-29 11:37:04 -0300

    Add function to plot 1d histogram and gaussians

--- histgmm/visualization.py
+++ histgmm/visualization.py
@@ -1,12 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from .core import HistogramGMM
+from .utils import gaussian_1d
 
 
 def compute_gaussian_amplitudes_on_histogram(
-    gmm: HistogramGMM,
-    X: np.ndarray, 
-    h: np.ndarray
+    gmm: HistogramGMM, X: np.ndarray, h: np.ndarray
 ) -> np.array:
     """Compute the amplitudes of the Gaussians on the histogram.
 
@@ -34,8 +34,42 @@ def compute_gaussian_amplitudes_on_histogram(
         index_closest = np.argmin(np.abs(X - gmm.means_[cluster]))
 
         inverse_gaussian_normalization_factor = np.sqrt(
-            (2*np.pi)**gmm.n_dims * np.linalg.det(gmm.covariances_[cluster])
+            (2 * np.pi) ** gmm.n_dims * np.linalg.det(gmm.covariances_[cluster])
         )
         amplitudes[cluster] = h[index_closest] * inverse_gaussian_normalization_factor
 
     return amplitudes
+
+
+def plot_1d_histogram_and_gaussians(
+    gmm: HistogramGMM,
+    X: np.ndarray,
+    h: np.ndarray,
+    ax=None,
+    component_std_extension=5,
+    n_points_for_gaussian=100,
+):
+    stds = component_std_extension
+    amplitudes = compute_gaussian_amplitudes_on_histogram(gmm=gmm, X=X, h=h)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.bar(X.squeeze(), h, width=0.1, color="lightgray")
+
+    for k in range(gmm.n_components):
+        color = plt.cm.viridis(k / gmm.n_components)
+        x_gaussian = np.linspace(
+            gmm.means_[k] - stds * np.sqrt(gmm.covariances_[k]),
+            gmm.means_[k] + stds * np.sqrt(gmm.covariances_[k]),
+            n_points_for_gaussian,
+        )
+        y_gaussian = gaussian_1d(
+            x_gaussian.squeeze(),
+            amplitudes[k].squeeze(),
+            gmm.means_[k].squeeze(),
+            gmm.covariances_[k].squeeze(),
+        )
+        ax.plot(x_gaussian.squeeze(), y_gaussian, color=color)
+
+    return ax
