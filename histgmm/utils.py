73947ef84cb9eaa49da1d6237b3b63c7823eab64
thomasvf <tvfontanari@gmail.com>
2024-03-05 21:18:47 -0300

    Add n-dimensional gaussian generation utilities

--- histgmm/utils.py
+++ histgmm/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 
 def gaussian_1d(x: np.ndarray, A: float, mu: float, var: float) -> np.ndarray:
@@ -34,3 +35,74 @@ def variance_for_n_std_for_all_range(X: np.ndarray, n_std: float=4) -> np.ndarra
     """Compute a variance such that `n_std` standard deviations 
     will occupy the full range of the data."""
     return ((X.max() - X.min())/n_std)**2
+
+
+def ndim_normal_pdf(x: np.ndarray, mu: np.ndarray, cov: np.ndarray) -> np.ndarray:
+    """Sample a normal distribution with mean `mu` and covariance `cov`.
+
+    Parameters
+    ----------
+    x : np.ndarray (*, n_dims)
+        Points where to compute the gaussian curve
+    mu : np.ndarray (n_dims, )
+        Mean of the gaussian
+    cov : np.ndarray (n_dims, n_dims)
+        Covariance matrix of the gaussian
+
+    Returns
+    -------
+    np.ndarray
+        Normal pdf value at the specified points
+    """
+    assert x.shape[-1] == len(mu), "last x dimension and mu must match"   
+    assert x.shape[-1] == cov.shape[0], "last x dimension must match with cov dimensions"
+
+    rv = stats.multivariate_normal(mean=mu, cov=cov)
+    return rv.pdf(x)
+
+
+def ndim_isotropic_normal_pdf(x: np.ndarray, mu: np.ndarray, var: float) -> np.ndarray:
+    """Sample a isotropic normal distribution with mean `mu` and variance `var`.
+
+    Parameters
+    ----------
+    x : np.ndarray (*, n_dims)
+        Points where to compute the gaussian curve
+    mu : np.ndarray (n_dims, )
+        Mean of the gaussian
+    var : float
+        Variance of the gaussian
+
+    Returns
+    -------
+    np.ndarray
+        Normal pdf value at the specified points
+    """
+    cov = np.eye(len(mu)) * var
+    return ndim_normal_pdf(x, mu, cov)
+
+
+def isotropic_normal_pdf_on_the_2d_grid(min_x: np.ndarray, max_x: np.ndarray, mu: np.ndarray, var:float):
+    """Sample a isotropic normal distribution with mean `mu` and variance `var` on a 2D grid
+    defined by `min_x` and `max_x`.
+
+    Parameters
+    ----------
+    min_x : np.ndarray (2,)
+        Diagonal vertex of the grid
+    max_x : np.ndarray (2,)
+        Other diagonal vertex of the grid
+    mu : np.ndarray
+        Mean of the gaussian
+    var : float
+        Variance of the isotrpoic gaussian
+
+    Returns
+    -------
+    (np.ndarray, np.ndarray)
+        Coordinates of each matrix element and normal pdf values at the specified points
+    """
+    x, y = np.mgrid[min_x[0]:max_x[0]:0.1, min_x[1]:max_x[1]:0.1]
+    pos = np.dstack((x, y))
+    probs = ndim_isotropic_normal_pdf(pos, mu, var)
+    return (pos, probs)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
+from scipy import stats
 
-from histgmm.utils import multidimensional_linspace
+from histgmm.utils import isotropic_normal_pdf_on_the_2d_grid, multidimensional_linspace
 
 
 @pytest.mark.parametrize("X, n_points, expected", [
@@ -12,3 +13,16 @@ from histgmm.utils import multidimensional_linspace
 def test_multidimensional_linspace(X, n_points, expected):
     computed = multidimensional_linspace(X, n_points)
     np.testing.assert_allclose(computed, expected, rtol=1e-5, atol=1e-8)
+
+
+def test_isotropic_normal_pdf_on_the_2d_grid():
+    rv1 = stats.multivariate_normal(mean=[-2.5, -2.5], cov=[[0.1, 0], [0, 0.1]])
+
+    x, y = np.mgrid[-5:5:0.1, -5:5:0.1]
+    pos = np.dstack((x, y))  
+    probs = rv1.pdf(pos)
+
+    pos_t, probs_t = isotropic_normal_pdf_on_the_2d_grid((-5, -5), (5, 5), [-2.5, -2.5], 0.1)
+    
+    np.testing.assert_allclose(pos, pos_t)
+    np.testing.assert_allclose(probs, probs_t)
